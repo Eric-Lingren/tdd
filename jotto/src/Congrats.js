dce20b1f0cea6fb16237255a4922ed9126e33744
Eric Lingren <ericlingren@gmail.com>
2019-04-19 14:47:56 -0600

    fixing congrats component bugs

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -10,7 +10,7 @@ import React from 'react';
 const Congrats = (props) => {
     if(props.success){
         return(
-            <div data-test='component'>
+            <div data-test='component-congrats'>
                 <span data-test='congrats-message'>
                     Congratulations!  You guessed the word!
                 </span>
