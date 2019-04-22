f6118518a6f3184f9c701095e8a48196206a307d
Eric Lingren <ericlingren@gmail.com>
2019-04-22 14:18:10 -0600

    added bootstrap cdn and some styling

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -6,9 +6,10 @@ import GuessedWords from './GuessedWords'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <Congrats />
-        <GuessedWords /> */}
+      <div className='container'>
+      <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
       </div>
     );
   }
--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -6,7 +6,7 @@ const GuessedWords = (props) => {
     if(props.guessedWords.length === 0){
         contents = (
             <span data-test='guess-instructions'>
-                Try to guess the word!
+                Try to guess the secret word!
             </span>
         )
     }else{
@@ -19,8 +19,8 @@ const GuessedWords = (props) => {
         contents = (
             <div data-test='guessed-words'>
                 <h3>Guessed Words</h3>
-                <table>
-                    <thead>
+                <table className='table table-sm'>
+                    <thead className='thead-light'>
                         <tr><th>Guess</th><th>Matching Letters</th></tr>
                     </thead>
                     <tbody>
@@ -30,7 +30,7 @@ const GuessedWords = (props) => {
             </div>
         )
     }
-    
+
     return(
         
         <div data-test='component-guessed-words'>
