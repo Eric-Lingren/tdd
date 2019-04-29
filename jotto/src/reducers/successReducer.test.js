701e15956138275485048cb5499b1d0a3aa31a91
Eric Lingren <ericlingren@gmail.com>
2019-04-29 09:41:09 -0600

    adding tests for redux action creators and reducers

---
+++ jotto/src/reducers/successReducer.test.js
@@ -0,0 +1,13 @@
+import { actionTypes } from '../actions'
+import successReducer from './successReducer'
+
+test('returns initial state of false when no action is passed', () => {
+    const newState = successReducer(undefined, {})
+    expect (newState).toBe(false)
+})
+
+test('returns state of true upon receiving an action type of `CORRECT_GUESS`', () => {
+    const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS })
+    expect(newState).toBe(true)
+})
+
